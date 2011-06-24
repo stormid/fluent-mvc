3a41fd9edf5eb24c995de08ceb4017082b96c72a
Rory Fitzpatrick <rory@stormid.com>
2011-06-24 13:51:09 +0100

    Updates System.Web.Mvc references to local

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,11 +2,11 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyDescription("Git commit hash: ae6c2e6baea584f4fd7ba546960ac934ee04f06f - 2010-06-23 111910 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
+[assembly: AssemblyDescription("Git commit hash: f186e37c21343b354d5972106040a5d5e0323cb3 - 2011-06-24 134356 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
 [assembly: AssemblyProduct("FluentMvc")]
 [assembly: AssemblyCopyright("Copyright 2009-2011 Chris Canal. All rights reserved.")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.11082")]
+[assembly: AssemblyFileVersion("0.1.0.11175")]
 
 [assembly: AssemblyInformationalVersion("0.1.0.0")]
 [assembly: ComVisibleAttribute(false)]
