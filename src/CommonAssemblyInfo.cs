2bdf6b5a5aa401a80dc348adba05a050bbd4bdd8
Chris Canal <chris.canal@stormid.com>
2011-07-27 17:16:59 +0100

    Updated version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,11 +2,11 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyDescription("Git commit hash: 3a41fd9edf5eb24c995de08ceb4017082b96c72a - 2011-06-24 135109 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
+[assembly: AssemblyDescription("Git commit hash: 5d25c6b9fc79c050ce526e6ea127b2c7e60e070c - 2011-07-15 103351 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
 [assembly: AssemblyProduct("FluentMvc")]
 [assembly: AssemblyCopyright("Copyright 2009-2011 Chris Canal. All rights reserved.")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.11180")]
+[assembly: AssemblyFileVersion("0.1.0.11196")]
 
 [assembly: AssemblyInformationalVersion("0.1.0.0")]
 [assembly: ComVisibleAttribute(false)]
