700b326df1e8c78ea1857b6eebfa5a60336fac44
Chris Canal <chris.canal@stormid.com>
2013-01-10 17:27:46 +0000

    Fixes typo

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,8 +2,8 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
-[assembly: AssemblyDescription("Git commit hash: 41c2c6e9a00f9f6e910a468b80fd7c5250b2b6b4 - 2013-01-09 165401 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
-[assembly: AssemblyProduct("FluentMvc")]
+[assembly: AssemblyDescription("Git commit hash: eefc5beb83d6d6347f468f06794dff9badaf6a6b - 2013-01-09 173814 - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
+[assembly: AssemblyProduct("Storm.FluentMvc")]
 [assembly: AssemblyCopyright("Copyright 2009-2011 Chris Canal. All rights reserved.")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.13009")]
